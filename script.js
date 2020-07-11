b03c04b803372153ff3fe69c50923abefba7ff45
jvst1 <61890677+jvst1@users.noreply.github.com>
2020-07-11 12:27:38 -0300

    Erro na colisao barras

--- script.js
+++ script.js
@@ -1,35 +1,66 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
-let x=canvas.width/2;
-let y=canvas.height-30;
-let dx=2;
-let dy=-2;
+let x;
+let y;
+let dx;
+let dy;
 let ballRadius=10;
+
+let wPressed= false;
+let sPressed= false;
+let upPressed= false;
+let downPressed=false;
 
 let paddleHeight = 55;
 let paddleWidth = 2;
-let paddleX= 507;
+let paddleX= 15;
 let paddleY= (canvas.height-paddleHeight)/2;
-let wPressed= false;
-let sPressed= false;
 
+let paddleX2= 1009;
+let paddleY2= (canvas.height-paddleHeight)/2;  
+// pontuação
+let score = 0;
+let score2 = 0;
 
-
-function drawPaddle(){
+function setupBallAndPaddle() {
+    x = canvas.width / 2; // inicial horizontal
+    y = canvas.height - 35; // inicial vertical
+    dx = 2; // variação horizontal
+    dy = -2; // variação vertical;
+    paddleY=  (canvas.height-paddleHeight)/2;
+    paddleY2= (canvas.height-paddleHeight)/2;  
+}
+setupBallAndPaddle();
+function drawPlayer1(){
     ctx.beginPath();
     ctx.rect(paddleX,paddleY,paddleWidth, paddleHeight);
     ctx.fillStyle = "#ddd";
     ctx.fill();
     ctx.closePath();
-
 }
-
+function drawPlayer2(){
+    ctx.beginPath();
+    ctx.rect(paddleX2,paddleY2,paddleWidth, paddleHeight);
+    ctx.fillStyle = "#ddd";
+    ctx.fill();
+    ctx.closePath();
+}
+function drawScore(){
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText(score, (canvas.width/2)-(canvas.width/4), (canvas.height/8));
+}
+function drawScore2(){
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText(score2, (canvas.width/2)+(canvas.width/4), (canvas.height/8));
+}
 function drawBall(){
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#ccc";
-    ctx.fill();
+    ctx.fill(); 
     ctx.closePath();
 }
 function drawLine(){
@@ -42,32 +73,41 @@ function drawLine(){
 let interval = setInterval(draw, 10);
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawPaddle();
     drawLine();
-    // Limite saida horizontal -
-    
-    if(x>paddleX && x<paddleX+paddleY && y+ballRadius>=paddleY){
-        dx= -dx;
-    }else if(x+dx<ballRadius || x + dx > canvas.width-ballRadius){
-        alert("Game Over!");
-        document.location.reload();
-        clearInterval(interval);
-    }
-    /*
-    if(x+dx<ballRadius){
+    drawBall();
+    drawPlayer1();
+    drawPlayer2();
+    drawScore();
+    drawScore2();
+   // verifica se a bola sai na vertical
+    // verifica se a bola sai na vertical
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy; // inverte o sinal de dx
+      }
+      // verifica se a bola sai na horizontal
+      if (
+        y > paddleY && y < paddleY + paddleHeight && // entre a barra (eixo x)
+        x + ballRadius >= paddleX || x > paddleX2 &&
+       x < paddleX2 + paddleWidth &&
+       y - ballRadius <= paddleY2 + paddleHeight) {
         dx = -dx;
-    }else if(x + dx > canvas.width-ballRadius){
-        alert("Game Over!");
+      }
+      else if (x + dx > canvas.width - ballRadius) {
+        score++;
+        setupBallAndPaddle();
+      }else if (x + dx < ballRadius){
+        score2++;
+        setupBallAndPaddle();
+      }
+    //condicao de vitoria
+    if(score == 5){
+        alert("Player 1 Ganhou!");
         document.location.reload();
-        clearInterval(interval);
-    }
-
-    */
-    if(y + dy > canvas.height-ballRadius || y+dy<ballRadius ){
-        dy = -dy;
-    }
-
+      }else if(score2 == 5){
+        alert("Player 2 Ganhou!");
+        document.location.reload();
+      }
+    //controles p1
     if(sPressed){
         paddleY+=7;
         if(paddleY + paddleHeight>canvas.height){
@@ -79,27 +119,49 @@ function draw(){
             paddleY=0;
         }
     }
-
+    //controles p2
+    if(downPressed){
+        paddleY2+=7;
+        if(paddleY2 + paddleHeight>canvas.height){
+            paddleY2=canvas.height-paddleHeight;
+        }
+    }else if(upPressed){
+        paddleY2-=7;
+        if(paddleY2<0){
+            paddleY2=0;
+        }
+    }
+    //variacao bola
     x+= dx;
     y+= dy;
 }
-
 function keyDownHandler(e){
     if(e.key =="W" || e.key=="w"){
         wPressed = true;
     }
+    if(e.key =="ArrowUp" || e.key=="Up"){
+        upPressed = true;
+    }
     if(e.key =="S" || e.key=="s"){
         sPressed = true;
+    }
+    if(e.key =="ArrowDown" || e.key=="Down"){
+        downPressed = true;
     }
 }
 function keyUpHandler(e){
     if(e.key =="W" || e.key=="w"){
         wPressed = false;
     }
+    if(e.key =="ArrowUp" || e.key=="Up"){
+        upPressed = false;
+    }
     if(e.key =="S" || e.key=="s"){
         sPressed = false;
     }
-}
-    
+    if(e.key =="ArrowDown" || e.key=="Down"){
+        downPressed = false;
+    }
+}  
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
